25c0dcbf21d7e3395bfcc1285af8d806f6391bb1
91630 <91630@DESKTOP-1RJFJM7>
2022-03-28 13:28:37 +0530

    amazon commit

---
+++ com.ecommerce.amazon/src/test/java/amazonsearch/SearchBarTest.java
@@ -0,0 +1,16 @@
+package amazonsearch;
+
+import org.testng.annotations.Test;
+
+public class SearchBarTest {
+	
+	@Test
+	public void entervaluesTest() {
+		System.out.println("entered values successfully");
+	}
+	@Test
+	public void entervaluesTest2() {
+		System.out.println("entered values successfully");
+	}
+
+}
